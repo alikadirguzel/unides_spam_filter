e7a1059e4530622fb17ede20655b014ae4aea033
yunusemre23023 <yunusemreg681@gmail.com>
2026-01-13 16:32:50 +0300

    Port 5002

--- app.py
+++ app.py
@@ -385,6 +385,6 @@ if __name__ == "__main__":
     import os
     debug_mode = os.getenv("FLASK_DEBUG", "False").lower() == "true"
     host = os.getenv("FLASK_HOST", "0.0.0.0")
-    port = int(os.getenv("FLASK_PORT", "5000"))
+    port = int(os.getenv("FLASK_PORT", "5002"))
     app.run(debug=debug_mode, host=host, port=port)
 
